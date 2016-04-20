34578cbbbd20fae0690e0b4d1d534c3e1e2d57b4
U-TANDEM\ckingsley <kingsley.chris@gmail.com>
2016-04-19 17:50:44 -0700

    week 2

---
+++ README.md
@@ -0,0 +1,1 @@
+Code repository for  HarvardX: PH525.4x Data Analysis for Life Sciences 4: High-Dimensional Data Analysis 
--- week2/week2.R
+++ week2/week2.R
@@ -1,1 +1,12 @@
 # week 2
+
+library(tissuesGeneExpression)
+data(tissuesGeneExpression)
+
+
+# problem 1
+s <- svd(e)
+m <- rowMeans(e)
+cor(s$u[,1], m)
+
+# problem 2
